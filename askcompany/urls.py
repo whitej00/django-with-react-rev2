c256bf222a31c249aa0dc0bd6bad1d332e7dd71e
whitej00 <68831140+whitej00@users.noreply.github.com>
2022-08-18 21:48:06 +0900

    initial commit

---
+++ askcompany/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document__rot=settings.MEDIA_ROOT)
